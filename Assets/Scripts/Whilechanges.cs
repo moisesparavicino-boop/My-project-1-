232b1c4526877bfa8acfe9de2d839d0fe3678450
moisesparavicino-boop <moises.paravicino@tecsup.edu.pe>
2026-05-11 09:26:49 -0500

    Initial commit

---
+++ Assets/Scripts/Whilechanges.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+using NUnit.Framework;
+public class Whilechanges : MonoBehaviour
+{
+    public List<string> nombres = new List<string>() { "A", "B", "C", "D", "E" };
+    void Start()
+    {
+        // Debug.Log(nombres[nombres-Count -1]};
+        int i = 0;
+
+        while (i < nombres.Count)
+        {
+            Debug.Log(nombres[i]);
+            if (nombres[i] == "C")
+            {
+                Debug.Log("El elemento C se encuentra en la posicion " + i);
+
+            }
+            i++;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
